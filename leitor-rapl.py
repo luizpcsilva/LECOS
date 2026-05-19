d90b3ad5d9a95118966594d75d503757424dcb82
Luiz Paulo <luizp.silva.2005@gmail.com>
2026-05-19 12:49:46 -0300

    adiciona leitor rapl
    lê dados dpo rapl por 10 segundos
    inicia script de estresse da cpu por 1 minuto, lendo os dados do rapl ao mesmo tempo
    lê dados do rapl por mais 10 segundos
    
    intervalo de amostragem: 1 segundo

---
+++ leitor-rapl.py
@@ -0,0 +1,49 @@
+import os
+import subprocess
+import time
+import string
+
+OUTPUT_NAME = "teste"
+STRESS_FUNC = "stress-ng --matrix 0 -t 1m"
+STRESS_FUNC = STRESS_FUNC.split()
+
+#retorna o valor do contador de microjoule de package como string
+def leitor_rapl():
+    rapl = open("/sys/class/powercap/intel-rapl/subsystem/intel-rapl:0/energy_uj", "r")
+    texto = rapl.readline() + "\n"
+    rapl.close()
+    return texto
+
+#cria o arquivo de output
+file = open("output/"+OUTPUT_NAME, "w")
+
+# faremos uma medição de 10 segundos, iniciaremos a função do stressng
+# por 1 minuto e, após o término, faremos mais 10 segundos de coleta
+
+texto = ""
+
+#10 segundos de testagem sem stress
+tempoInicio = time.perf_counter()
+tempo = tempoInicio
+while (tempo - tempoInicio <= 10): 
+    texto += leitor_rapl()
+    time.sleep(1)
+    tempo = time.perf_counter()
+    
+#inicia stressng
+stress = subprocess.Popen(STRESS_FUNC)
+while(stress.poll() == None):
+    texto += leitor_rapl()
+    time.sleep(1)
+    tempo = time.perf_counter()
+
+#10 segundos de testagem sem stress
+tempoInicio = time.perf_counter()
+tempo = tempoInicio
+while (tempo - tempoInicio <= 10): 
+    texto += leitor_rapl()
+    time.sleep(1)
+    tempo = time.perf_counter()
+
+file.write(texto)
+file.close()
