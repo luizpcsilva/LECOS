29ff0b03812076840e35edd129f48bbad2d40c17
Luiz Paulo <luizp.silva.2005@gmail.com>
2026-07-18 01:26:16 -0300

    adiciona requirements.txt e script de medicao com codecarbon

--- docs/06_emissoes_codecarbon.md
+++ docs/06_emissoes_codecarbon.md
@@ -1,2 +1,4 @@
+
+
 ## Navegação
 [⬅️ Passo Anterior: Medicao de Processos com Scaphandre](05_medicao_scaphandre.md)
---
+++ scripts/medicao-codecarbon.py
@@ -0,0 +1,26 @@
+import os
+import subprocess
+import time
+import argparse
+#cria contador para codecarbon
+from codecarbon import OfflineEmissionsTracker
+
+
+#configuração dos argumentos passados via terminal
+parser = argparse.ArgumentParser(description="")
+parser.add_argument("func1", type=str, help="codigo para chamar função 1 do stress ng")
+parser.add_argument("freq", type=float, help="frequencia da amostragem do rapl (em segundos)")
+parser.add_argument("caminhoOutput", type=str, help="nome do arquivo para salvar resultados")
+args = parser.parse_args()
+
+args.func1 = args.func1.split()
+
+#--------------------- Inicio Medição ----------------------
+
+with OfflineEmissionsTracker(country_iso_code="BRA", measure_power_secs=args.freq, output_dir="output/", output_file=args.caminhoOutput, log_level="error") as tracker:
+    #inicia stressng
+    stress = subprocess.Popen(args.func)
+    while(stress.poll() == None):
+        time.sleep(args.freq)
+
+#--------------------- Fim Medição -------------------------
