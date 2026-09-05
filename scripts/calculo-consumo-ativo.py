b6ad555d64ddde2d2a92be86cf1043cd988d491b
Luiz Paulo <luizp.silva.2005@gmail.com>
2026-09-05 17:55:17 -0300

    corrige nome de argumento

--- scripts/calculo-consumo-ativo.py
+++ scripts/calculo-consumo-ativo.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 #configuração dos argumentos passados via terminal
 parser = argparse.ArgumentParser(description="")
-parser.add_argument("média_consumo_total", type=float, help="media do consumo total registrado")
+parser.add_argument("media_consumo_total", type=float, help="media do consumo total registrado")
 parser.add_argument("media_residual", type=float, help="media do consumo residual total")
 
 args = parser.parse_args()
