14a10b1aaee20247ce9bb90a6437e2fa176e5e31
Luiz Paulo <luizp.silva.2005@gmail.com>
2026-09-14 14:49:06 -0300

    altera chamada de medição residual

--- valores.sh
+++ valores.sh
@@ -8,17 +8,17 @@ export TIPO_ESTRESSOR_1=
 export TIPO_ESTRESSOR_2=
 
 #para o estressor utilizado no consumo residual, pode escrever o comando todo (sem o taskset, apenas stress-ng)
-export ESTRESSOR_RESIDUAL=
+export ESTRESSOR_RESIDUAL="stress-ng --cpu 1 -t 30"
 
 #medicoes -> variavel vazia força uma nova medição
 export CONSUMO_RESIDUAL=
-export BASELINE_P1=
-export BASELINE_P2=
+export BASELINE_P1=1
+export BASELINE_P2=1
 #se o baseline acima nao tiver valor, o script irá calcula-lo com os valores abaixo
-export CONSUMO_ATIVO_P1=
-export CONSUMO_ATIVO_P2=
-export CONSUMO_ATIVO_P1_P2=
+export CONSUMO_ATIVO_P1=1
+export CONSUMO_ATIVO_P2=1
+export CONSUMO_ATIVO_P1_P2=1
 
 #configurações
-export DO_ISOLAMENTO="0"   #1 para isolar e 0 para nao isolar
-export DO_RESFRIAMENTO="0" #1 para aplicar timer de resfriamento entre medições e 0 para não fazer isso
+export DO_ISOLAMENTO="1"   #1 para isolar e 0 para nao isolar
+export DO_RESFRIAMENTO="1" #1 para aplicar timer de resfriamento entre medições e 0 para não fazer isso
