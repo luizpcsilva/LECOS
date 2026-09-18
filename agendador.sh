19ae01b78d77c4f69a1e67ace18e66d62434f0e5
Luiz Paulo <luizp.silva.2005@gmail.com>
2026-09-17 21:23:52 -0300

    atualiza metodo float64 para float

--- agendador.sh
+++ agendador.sh
@@ -6,7 +6,7 @@ ESTRESSORES=(
     "stress-ng --cpu 1 --cpu-method ackermann -t 30"
     "stress-ng --cpu 1 --cpu-method queens -t 30" 
     "stress-ng --cpu 1 --cpu-method fibonacci -t 30" 
-    "stress-ng --cpu 1 --cpu-method float64 -t 30" 
+    "stress-ng --cpu 1 --cpu-method float -t 30" 
     "stress-ng --cpu 1 --cpu-method int64 -t 30" 
     "stress-ng --cpu 1 --cpu-method decimal64 -t 30" 
     "stress-ng --cpu 1 --cpu-method double -t 30" 
